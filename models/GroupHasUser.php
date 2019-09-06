a87f473c2c3a8b67c7caeb32119105805c64017f
dead0343@gmail.com <s148554piy143>
2019-09-06 14:56:43 +0300

    Добавлена модель groupHasUser

---
+++ models/GroupHasUser.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "group_has_user".
+ *
+ * @property int $id
+ * @property int $group_id
+ * @property int $user_id
+ *
+ * @property Group $group
+ * @property User $user
+ */
+class GroupHasUser extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'group_has_user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['group_id', 'user_id'], 'required'],
+            [['group_id', 'user_id'], 'integer'],
+            [['group_id'], 'exist', 'skipOnError' => true, 'targetClass' => Group::className(), 'targetAttribute' => ['group_id' => 'id']],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'group_id' => Yii::t('app', 'Group ID'),
+            'user_id' => Yii::t('app', 'User ID'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getGroup()
+    {
+        return $this->hasOne(Group::className(), ['id' => 'group_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+}
